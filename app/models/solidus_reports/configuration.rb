243c4d56e0c63b142517dc6e5c0613f74a3642e1
tyler <84726170+embold-tyler@users.noreply.github.com>
2025-08-01 11:03:52 -0700

    :recycle: Refactor menu item creation logic for Solidus < 4 version compatibility

--- app/models/solidus_reports/configuration.rb
+++ app/models/solidus_reports/configuration.rb
@@ -2,13 +2,23 @@
 
 module SolidusReports
   class Configuration < Spree::Preferences::Configuration
-    REPORT_TABS ||= [:reports].freeze
+    # Solidus 4.0 and later use a different way to define menu items
+    if Gem::Version.new(Spree.solidus_version) >= Gem::Version.new('4.0')
+      new_item = Spree::BackendConfiguration::MenuItem.new(
+        label: "Reports",
+        icon: "file",
+        condition: -> { can?(:admin, :reports) }
+      )
+    else
+      REPORT_TABS ||= [:reports].freeze
 
-    new_item = Spree::BackendConfiguration::MenuItem.new(
-      label: "Reports",
-      icon: "file",
-      condition: -> { can?(:admin, :reports) },
-    )
+      new_item = Spree::BackendConfiguration::MenuItem.new(
+        REPORT_TABS,
+        "file",
+        condition: -> { can?(:admin, :reports) }
+      )
+    end
+
     Spree::Backend::Config.menu_items << new_item
   end
 end
